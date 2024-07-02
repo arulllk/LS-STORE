89cd0f4093a5b573faa9eeca9877bfd1a6ad7c78
Arul <arul.anthony@digivationworld.com>
2024-07-03 00:01:04 +0530

    sign in page

--- admin/src/page/SignIn.jsx
+++ admin/src/page/SignIn.jsx
@@ -8,12 +8,28 @@ const SignIn = () => {
                 <h3 className='text-2xl font-bold text-dreamless-sleep'>Login to account</h3>
                 <p className='text-black-water'>Enter your email & password to login</p>
             </div>
-            <form>
+            <form className='flex flex-col gap-6'>
                 <fieldset className='flex flex-col '>
-                    <label className='mb-10'>Email address </label>
+                    <label className='mb-10 font-bold text-sm'>Email address <span className='text-red-600'>*</span></label>
                     <input className='border-powder-white border-solid border-1 h-12 rounded-xl px-3 py-5 text-sm' type="email" name="email" placeholder='Enter your email address' value='' />
                 </fieldset>
+
+                <fieldset className='flex flex-col '>
+                    <label className='mb-10 font-bold text-sm'>Password <span className='text-red-600'>*</span></label>
+                    <input className='border-powder-white border-solid border-1 h-12 rounded-xl px-3 py-5 text-sm' type="email" name="email" placeholder='Enter your password' value='' />
+                </fieldset>
+                <div className='flex justify-between items-center'>
+                  <div className="flex gap-10">
+                    <input type='checkbox' id="signed" />
+                    <label className='text-sm text-black-water'>Keep me signed in</label>
+                  </div>
+
+                  <a href='/' className='text-sm text-bolt-blue'>Forgot password?</a>
+                </div>
+                <a href='/' className='btn-custom'>Login</a>
+
             </form>
+            <p className='text-sm text-center' text-black-water>You don't have an account yet? <a href='/' className='text-bolt-blue'>Register Now</a> </p>
        </div>
     </div>
   )
--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -11,7 +11,8 @@ module.exports = {
       colors: {
         'dreamless-sleep': '#111111',
         'black-water': '#575864',
-        'powder-white': '#ecf0f4'
+        'powder-white': '#ecf0f4',
+        'bolt-blue': '#2275fc'
       },
       margin:{
         '10':'.625rem'
@@ -21,10 +22,42 @@ module.exports = {
       },
       borderWidth:{
         '1':'.0625rem'
-      }
+      },
+      gap:{
+        '10':'.625rem'
+      },
 
     },
   },
-  plugins: [],
+  plugins: [
+    function ({addUtilities}) {
+      const newUtilities = {
+        '.btn-custom' : {
+          backgroundColor: '#2275fc',
+          border: '1px solid #2275fc',
+          height: '50px',
+          padding: '15px 22px',
+          color: '#fff',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: '8px',
+          fontFamily: 'Inter, sans-serif',
+          fontSize: '14px',
+          fontWeight: '700',
+          lineHeight: '20px',
+          backgroundSize: '100%',
+          overflow: 'hidden',
+          borderRadius: '12px',
+          transition: 'all .3s ease'
+        },
+        '.btn-custom:hover': {
+          backgroundColor: '#ffffff',
+          color: '#2275fc',
+        }, 
+      }
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
 }
 
