1dd9d6846a15887fba48d492fb1b188e557ac703
Arul <arul.anthony@digivationworld.com>
2024-08-11 17:34:30 +0530

    added

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,20 @@
 import { Routes,Route} from 'react-router-dom';
 import SignIn from './page/SignIn';
 import SignUp from './page/SignUp';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <SignUp />
+    // <Routes>
+    //   <Route path='/signup' element={<SignUp />} />
+    //   <Route path='/signin' element={<SignIn />} />
+    // </Routes>
+    <Routes>
+        <Route element={<Layout />}  >
+
+        </Route>
+    </Routes>
+    
   );
 }
 
